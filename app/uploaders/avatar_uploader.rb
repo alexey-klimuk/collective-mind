34bf7596b1c653c12f0c99f7a227e319659cd145
Alexey Klimuk <alexey.klimuk@itechart-group.com>
2013-09-03 17:01:52 +0300

    customize users page

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -20,12 +20,13 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+     # For Rails 3.1+ asset pipeline compatibility:
+     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+
+     #"/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    assets_path [version_name, 'default.gif'].compact.join('_')
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
@@ -46,7 +47,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(jpg jpeg png)
+    %w(jpg jpeg png gif)
   end
 
   # Override the filename of the uploaded files:
@@ -54,5 +55,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  def assets_path(img)
+    ActionController::Base.helpers.asset_path(img)
+  end
 
 end
